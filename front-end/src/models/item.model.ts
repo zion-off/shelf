c3b36a30e8aa30b42088011fd2db15be6864957d
zion <atibjawad.zion@optimizely.com>
2024-12-31 03:33:11 +0600

    first commit

---
+++ front-end/src/models/item.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { IItem } from "../interfaces/item.interface";
+
+const itemSchema = new mongoose.Schema<IItem>({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  notes: { type: String },
+  link: { type: String },
+  read: { type: Boolean, default: false },
+  in_folder: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Folder",
+    default: null,
+  },
+  created_at: { type: Date, default: Date.now },
+  last_modified: { type: Date, default: Date.now },
+});
+
+// Composite index on owner and in_folder
+itemSchema.index({ owner: 1, in_folder: 1 });
+
+const Item = mongoose.models.Item || mongoose.model<IItem>("Item", itemSchema);
+
+export default Item;
---
+++ front-end/src/models/user.model.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+
+const userSchema = new mongoose.Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  created_at: { type: Date, default: Date.now },
+});
+
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
