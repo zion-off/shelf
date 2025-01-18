967cac10d7cf6985a3b0297da84263c731d53e62
zion <atibjawad.zion@optimizely.com>
2025-01-19 05:28:52 +0600

    change sidebar icon

--- src/components/sidebar/appSidebar.tsx
+++ src/components/sidebar/appSidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { GalleryVerticalEnd } from "lucide-react"
+import { Book } from "lucide-react"
 
 import {
   Sidebar,
@@ -164,7 +164,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <GalleryVerticalEnd className="size-4" />
+                  <Book className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-semibold">Documentation</span>
