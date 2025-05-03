c2d6d67ac0aacc4d6d9649901275983c3b6d873d
zion <atibjawad.zion@optimizely.com>
2025-05-03 21:50:26 +0600

    fix linter errors

--- src/components/search/searchBar.tsx
+++ src/components/search/searchBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import Image from 'next/image';
 import { motion } from 'motion/react';
