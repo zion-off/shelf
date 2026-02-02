1aa35ff1ab6ea81b7cbe4af70e2d31ce72284482
zion <zion@nyu.edu>
2026-02-02 06:52:38 +0600

    add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,15 @@ export const metadata: Metadata = {
     statusBarStyle: "black-translucent",
     title: "Shelf",
   },
+  icons: {
+    icon: [
+      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    shortcut: "/favicon.ico",
+    apple: { url: "/apple-touch-icon.png", sizes: "180x180" },
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
